0f522633b9754efb51b38913e6e5568156290798
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-02 21:26:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import sys
+
 import pygame as pg
 
 if __name__ == '__main__':
-    print("Hi")
+    pg.init()
+
+    display = pg.display.set_mode((1600, 800))
+    clock = pg.time.Clock()
+
+    while True:
+        display.fill((0,0,0))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+
+        clock.tick(60)
+        pg.display.update()
