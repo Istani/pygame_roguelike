bcba2663c22c672ac106c68a0c126c0393e552ec
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-03 18:47:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from src.player import Player
 
 import pygame as pg
 
@@ -7,13 +8,15 @@ if __name__ == '__main__':
 
     display = pg.display.set_mode((1600, 800))
     clock = pg.time.Clock()
+    player = Player(800, 400, 32, 32)
 
     while True:
-        display.fill((0,0,0))
+        display.fill((0, 0, 0))
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
+        player.draw(display)
 
         clock.tick(60)
         pg.display.update()
