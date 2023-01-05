a9e394d58a08e423f7160f02c29c91b1c0a870bb
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-05 18:38:51 +0100

    Update projectile.py

--- src/projectile.py
+++ src/projectile.py
@@ -1,6 +1,7 @@
 import pygame.draw
 import math
 
+
 class Projectile:
 
     def __init__(self, x, y, x_mouse, y_mouse, color=(255, 0, 0), speed=15, size=5):
@@ -11,11 +12,33 @@ class Projectile:
         self.color = color
         self.speed = speed
         self.size = size
-        self.angle = math.atan2(y-y_mouse, x - x_mouse)
+        self.angle = math.atan2(y - y_mouse, x - x_mouse)
         self.x_vel = math.cos(self.angle) * self.speed
         self.y_vel = math.sin(self.angle) * self.speed
+        self.alive = True
+        self.animation_images = [pygame.image.load(f"../assets/projectiles/fire_bullet_{i}.png") for i in range(1, 4)]
+        self.animation_images = [pygame.transform.scale(i, (self.size, self.size)) for i in self.animation_images]
+        self.__animation_delay = 30
 
-    def draw(self, display):
+        self.mask = pygame.mask.from_surface(self.animation_images[0])
+        self.__animation_index = 0
+        self.animation_counter = 0
+
+    def draw(self, display, enemies):
+        if not self.alive:
+            return
+        for e in enemies:
+            offset_x = self.x - e.x
+            offset_y = self.y - e.y
+            if e.mask.overlap(self.mask, (offset_x, offset_y)) is not None:
+                self.alive = False
         self.x -= int(self.x_vel)
         self.y -= int(self.y_vel)
-        pygame.draw.circle(display, self.color, (self.x, self.y), self.size)
+
+        if self.__animation_index >= len(self.animation_images):
+            self.__animation_index = 0
+        display.blit(self.animation_images[self.__animation_index], (self.x, self.y))
+        self.animation_counter += 1
+        if self.animation_counter > self.__animation_delay:
+            self.__animation_index += 1
+            self.animation_counter = 0
