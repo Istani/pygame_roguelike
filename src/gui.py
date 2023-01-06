43e974fa5076636b758b92e5d9268a7a380e7786
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-06 20:31:59 +0100

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -3,11 +3,27 @@ import pygame
 
 class GUI:
 
-    def __init__(self, display, player):
+    def __init__(self, display, player, center):
         self.main_font = pygame.font.SysFont("comicsans", 20)
+        self.game_over_font = pygame.font.SysFont("comicsans", 100)
+
         self.display = display
         self.player = player
+        self.center = center
 
     def draw(self):
+        if not self.player.alive:
+            game_over = self.game_over_font.render("GAME OVER!", True, (255, 255, 255))
+            self.display.blit(game_over, (self.center[0]-400, self.center[1]-250))
+            self.display.blit(self.main_font.render(str(self.player.kills) + " kills", True, (255, 255, 255)), (self.center[0], self.center[1]))
+            return
         player_name = self.main_font.render(self.player.name, True, (255, 255, 255))
-        self.display.blit(player_name, (self.player.x - 15, self.player.y -28))
+        kills = self.main_font.render(str(self.player.kills), True, (255, 255, 255))
+        if self.player.live < 100:
+            c = (255, 0, 0)
+        else:
+            c = (255, 255, 255)
+        live = self.main_font.render(str(self.player.live) + " /100 HP", True, c)
+        self.display.blit(player_name, (self.player.x - 15, self.player.y - 28))
+        self.display.blit(live, (self.player.x - 15, self.player.y - 48))
+        self.display.blit(kills, (50, 50))
