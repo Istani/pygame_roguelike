dfdfa48ff3d986045a6f45805212c85b32b7d0df
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-07 18:54:38 +0100

    Update world.py

--- src/world.py
+++ src/world.py
@@ -1,22 +1,37 @@
+
 class World:
 
     def __init__(self):
-        self.player = None
         self.enemies = []
-        self.player_p
+        self.players = []
+        self.projectiles = []
+        self.trees = []
 
-    @staticmethod
-    def is_collision(projectile, enemy, display_scroll):
-        offset_x = projectile.x - enemy.x + display_scroll[0]
-        offset_y = projectile.y - enemy.y + display_scroll[1]
-        if projectile.mask.overlap(enemy.mask, (offset_x, offset_y)) is not None:
-            return True
-        return False
-
-    def check_collisions(self, enemies, projectiles, display_scroll):
-        for projectile in projectiles:
-            for enemy in enemies:
-                if self.is_collision(enemy=enemy, projectile=projectile, display_scroll=display_scroll):
+    def check_collisions(self, display_scroll):
+        for projectile in self.projectiles:
+            for enemy in self.enemies:
+                offset_x = projectile.x - enemy.x + display_scroll[0]
+                offset_y = projectile.y - enemy.y + display_scroll[1]
+                if projectile.mask.overlap(enemy.mask, (offset_x, offset_y)) is not None:
+                    enemy.hit_sound.play()
                     enemy.alive = False
                     projectile.alive = False
                     projectile.player.kills += 1
+
+        for player in self.players:
+            for enemy in self.enemies:
+                offset_x = player.x - enemy.x + display_scroll[0]
+                offset_y = player.y - enemy.y + display_scroll[1]
+                if player.mask.overlap(enemy.mask, (offset_x, offset_y)) is not None:
+                    player.live -= 1
+
+    def draw(self, display_scroll_x, display_scroll_y):
+        for tree in self.trees:
+            tree.draw(display_scroll_x, display_scroll_y)
+        for projectile in self.projectiles:
+            projectile.draw()
+        for enemy in self.enemies:
+            enemy.draw(display_scroll_x, display_scroll_y)
+        for player in self.players:
+            player.draw()
+
