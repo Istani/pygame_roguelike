63be9f652e4915e673fb7331e8527d449e7271f7
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-07 19:20:20 +0100

    Update tree.py

--- src/tree.py
+++ src/tree.py
@@ -5,6 +5,10 @@ class Tree:
         self.display = display
         self.x = x
         self.y = y
+        self.alive = True
+        self.live = 100
 
     def draw(self, display_scroll_x, display_scroll_y):
+        if not self.alive:
+            return
         self.display.blit(self.image, (self.x - display_scroll_x, self.y - display_scroll_y, 16, 16))
