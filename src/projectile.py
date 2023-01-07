bce3cbba040a449dabf34f592793dcd391d7a103
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-07 18:54:29 +0100

    Update projectile.py

--- src/projectile.py
+++ src/projectile.py
@@ -4,15 +4,15 @@ import math
 
 class Projectile:
 
-    def __init__(self, x, y, x_mouse, y_mouse, player, color=(255, 0, 0), speed=15, size=5):
-        self.x = x
-        self.y = y
+    def __init__(self, x_mouse, y_mouse, player, color=(255, 0, 0), speed=15, size=5):
+        self.x = int(player.x)
+        self.y = int(player.y)
         self.x_mouse = x_mouse
         self.y_mouse = y_mouse
         self.color = color
         self.speed = speed
         self.size = size
-        self.angle = math.atan2(y - y_mouse, x - x_mouse)
+        self.angle = math.atan2(self.y - y_mouse, self.x  - x_mouse)
         self.x_vel = math.cos(self.angle) * self.speed
         self.y_vel = math.sin(self.angle) * self.speed
         self.alive = True
@@ -24,23 +24,17 @@ class Projectile:
         self.__animation_index = 0
         self.animation_counter = 0
         self.player = player
+        self.display = player.display
 
-    def draw(self, display, display_scroll=None):
-        if display_scroll is None:
-            display_scroll = [0, 0]
+    def draw(self):
         if not self.alive:
             return
-        # for e in enemies:
-        #    offset_x = self.x - e.x
-        #    offset_y = self.y - e.y
-        #    if e.mask.overlap(self.mask, (offset_x, offset_y)) is not None:
-        #        self.alive = False
         self.x -= int(self.x_vel)
         self.y -= int(self.y_vel)
 
         if self.__animation_index >= len(self.animation_images):
             self.__animation_index = 0
-        display.blit(self.animation_images[self.__animation_index], (self.x, self.y))
+        self.display.blit(self.animation_images[self.__animation_index], (self.x, self.y))
         self.animation_counter += 1
         if self.animation_counter > self.__animation_delay:
             self.__animation_index += 1
