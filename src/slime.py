2eda099bf8c736f19bc1715fe713eb9b63dae5ed
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-07 15:14:23 +0100

    Update slime.py

--- src/slime.py
+++ src/slime.py
@@ -8,6 +8,8 @@ class Slime:
         self.x = x
         self.y = y
         self.animation_images = [pygame.image.load(f"../assets/slime/slime_animation_{i}.png") for i in range(4)]
+        self.animation_images = [pygame.image.load(f"../assets/valu/l0_sprite_{i}.png") for i in range(1, 6)]
+
         self.animation_count = 0
         self.reset_offset = 0
         self.offset_x = random.randrange(-300, 300)
@@ -16,16 +18,19 @@ class Slime:
         self.display = display
         self.alive = True
 
-    def draw(self, display_scroll, player, player_projectiles):
+    def draw(self, display_scroll, player, player_projectiles, aua):
         if not self.alive:
             return
         for p in player_projectiles:
             offset_x = p.x - self.x + display_scroll[0]
             offset_y = p.y - self.y + display_scroll[1]
             if p.mask.overlap(self.mask, (offset_x, offset_y)) is not None:
+                player.width += 1
+                player.height += 1
                 self.alive = False
                 p.alive = False
                 player.kills += 1
+                aua.play()
 
         # player dmg?
         offset_x = player.x - self.x + display_scroll[0]
@@ -55,5 +60,5 @@ class Slime:
         elif player.y + self.offset_y < self.y - display_scroll[1]:
             self.y -= 1
 
-        self.display.blit(pygame.transform.scale(self.animation_images[self.animation_count // 4], (32, 30)),
+        self.display.blit(pygame.transform.scale(self.animation_images[self.animation_count // 4], (64, 64)),
                           (self.x - display_scroll[0], self.y - display_scroll[1]))
