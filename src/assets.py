b3023a6a1f4e9ea0349e15d7030f6eeaf3c08da0
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-07 19:20:09 +0100

    Update assets.py

--- src/assets.py
+++ src/assets.py
@@ -10,9 +10,13 @@ class Assets:
         self.peng_sound = pygame.mixer.Sound("../assets/sounds/peng.wav")
         self.hit_sound = pygame.mixer.Sound("../assets/sounds/aua.wav")
         self.tree = pygame.image.load("../assets/environment/tree.png")
+        self.projectile_images = [pygame.image.load(f"../assets/projectiles/fire_bullet_{i}.png") for i in range(1, 4)]
+
         self.preprocess_images()
 
     def preprocess_images(self):
         self.tree = pygame.transform.scale(self.tree, (150, 250))
         self.player_images = [pygame.transform.scale(img, (42, 52)) for img in self.player_images]
         self.slime_images = [pygame.transform.scale(img, (32, 32)) for img in self.slime_images]
+        self.projectile_images = [pygame.transform.scale(img, (20, 20)) for img in self.projectile_images]
+
