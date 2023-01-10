d9ed2c9fb1588deab143940c5977735089948de6
Sandro <31536101+sandromund@users.noreply.github.com>
2023-01-10 20:59:25 +0100

    Update tilesheet.py

--- src/tilesheet.py
+++ src/tilesheet.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 
@@ -10,6 +12,23 @@ class TileSheet:
         self.n_columns = n_columns
         self.scale = scale
         self.tile_table = self.get_tile_table()
+
+        # random grass
+        self.grass_tile_row = random.randint(0, 9)
+        self.grass_tile_column = random.randint(0, 10)
+
+        self.n_random_objects = 5
+        self.objects = []
+        # 5 random sprites for the env
+        for _ in range(self.n_random_objects):
+            r = random.randint(11, 13)
+            if r == 11:
+                c = random.randint(10, 24)
+            else:
+                c = random.randint(0, 24)
+            self.objects.append((c, r))
+        self.object_draw = None
+
 
     def get_tile_table(self):
         tile_table = []
@@ -24,8 +43,31 @@ class TileSheet:
             tile_table.append(row)
         return tile_table
 
-    def draw(self, screen):
+    def draw(self, screen, display_scroll_x, display_scroll_y):
         #  this function is just for development test and will be removed soon.
-        for x, row in enumerate(self.tile_table):
-            for y, tile in enumerate(row):
-                screen.blit(tile, (x * 72, y * 72))
+        s = 32
+        n_row = int(pygame.display.get_window_size()[0] / s)
+        n_cols = int(pygame.display.get_window_size()[1] / s)
+        # draw all sprites
+        for x in range(n_row):
+            for y in range(n_cols):
+                tile_x = x * s - display_scroll_x
+                tile_y = y * s - display_scroll_y
+                screen.blit(self.tile_table[self.grass_tile_row][self.grass_tile_column],
+                            (tile_x, tile_y))
+
+        if self.object_draw is None:
+            self.object_draw = []
+            for x in range(n_row):
+                for y in range(n_cols):
+                    if random.random() < 0.05:
+                        tile_x = x * s - display_scroll_x
+                        tile_y = y * s - display_scroll_y
+                        tile_row, tile_column = self.objects[random.randint(0, self.n_random_objects - 2)]
+                        print(tile_row, tile_column)
+                        self.object_draw.append(
+                            (self.tile_table[tile_row][tile_column], (tile_x, tile_y))
+                        )
+        else:
+            for obj in self.object_draw:
+                screen.blit(obj[0], (obj[1][0] - display_scroll_x , obj[1][1] - display_scroll_y))
